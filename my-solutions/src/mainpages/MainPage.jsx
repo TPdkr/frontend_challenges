9c038b9a52339bd9e7f36b9e00eb74bac88ba8c3
TPdkr <tpodko@gmail.com>
2025-12-29 09:15:32 +0500

    main page update

--- my-solutions/src/mainpages/MainPage.jsx
+++ my-solutions/src/mainpages/MainPage.jsx
@@ -1,11 +1,15 @@
-import styles from './mainpage.module.css'
-import { Link } from 'react-router-dom'
+import styles from './mainpage.module.css';
+import { Link } from 'react-router-dom';
+import reactLogo from './assets/react.svg';
 
 function Mainpage() {
     return (
         <div className={styles.mainPage}>
             <div className={styles.title}>
-                <h1>Hello! Fancy seeing some solutions?</h1>
+                <div className={styles.logoRow}>
+                    <img src={reactLogo}/>
+                    <h1>Hello! Fancy seeing some solutions?</h1>
+                </div>
                 <h3>
                     My solutions to front-end mentor challenges grouped into a single repo.
                     This uses react and GitHub pages for creating the websites and for hosting it on the web.
